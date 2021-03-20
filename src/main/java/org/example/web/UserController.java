0e50057322f3c215124e0d887aecca3b1ef14eab
Danil <soundbanana1@gmail.com>
2021-03-20 20:05:32 +0400

    edit-and-delete

--- src/main/java/org/example/web/UserController.java
+++ src/main/java/org/example/web/UserController.java
@@ -19,7 +19,6 @@ public class UserController {
     public String addUser(@RequestParam String email, String login, String password) {
         User user = new User(email, login, password);
         userList.add(user);
-        System.out.println(userList);
 
         return "redirect:/user";
     }
@@ -35,15 +34,10 @@ public class UserController {
         return "all_users";
     }
 
-    @PostMapping("/delete_user/{numToDelete}")
-    public String deleteUser(@PathVariable String numToDelete) {
-        System.out.println("doing /delete_user");
-        System.out.println(numToDelete);
-
+    @PostMapping("/delete_user/{userToDelete}")
+    public String deleteUser(@PathVariable String userToDelete) {
         for (int i = 0; i < userList.size(); i++) {
-            System.out.println(userList.get(i).email);
-            System.out.println(1);
-            if (userList.get(i).email.equals(numToDelete)){
+            if (userList.get(i).email.equals(userToDelete)) {
                 userList.remove(i);
                 break;
             }
@@ -51,5 +45,29 @@ public class UserController {
         return "redirect:/user/all_users";
     }
 
+    @PostMapping("/edit_user/{userToEdit}")
+    public String editUser(@PathVariable String userToEdit, Model model) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).email.equals(userToEdit)) {
+                model.addAttribute("user", userList.get(i));
+                return "edit_user";
+            }
+        }
+        return "redirect:/user/all_users";
+    }
+
+    @PostMapping("/submit_edited_user/{old_email}")
+    public String submitEditedUser(@PathVariable String old_email,
+                                   @RequestParam String email, String login, String password) {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).email.equals(old_email)) {
+                userList.get(i).email = email;
+                userList.get(i).login = login;
+                userList.get(i).password = password;
+            }
+        }
+
+        return "redirect:/user/all_users";
+    }
 
 }
